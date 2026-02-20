263fdcb832d60efa1b842c471cb46a6740750b09
Roman Beniuk <executwbxrn@gmail.com>
2026-02-20 09:45:55 +0000

    add: routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:solid_software_test_application/src/core/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(routes: ref.read(routerProvider));
   }
 }

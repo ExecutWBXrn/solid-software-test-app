2adfd6694e37cbb82cd223c5aa68b148660ff3a8
Roman Beniuk <executwbxrn@gmail.com>
2026-02-20 10:19:01 +0000

    ref: home_screen ConsumerWidget -> ConsumerStatefulWidget

--- lib/src/features/home/presentation/screens/home_screen.dart
+++ lib/src/features/home/presentation/screens/home_screen.dart
@@ -2,11 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import '../providers/providers.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
     final backgroundColorValue = ref.watch(backgroundColorNotifierProvider);
     final backgroundColorNotifier = ref.read(
       backgroundColorNotifierProvider.notifier,
